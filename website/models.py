9503d145adb143d7c8f7502ca6d6717d330db469
diegosno <dserranoleal@gmail.com>
2023-08-04 18:13:32 +0200

    Implement New model with fields: title, slug, content, date_created, author, featured_image, excerpt, and status. Add status choices and set default value for the status field. Define Meta class to order the model by date_created. Implement str method to return the title.

--- website/models.py
+++ website/models.py
@@ -4,9 +4,21 @@ from cloudinary.models import CloudinaryField
 from django.utils import timezone
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
 
 class New(models.Model):
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(max_length=40, unique=True)
     content = models.TextField()
-    dated_created = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = CloudinaryField('image', default = "placeholder")
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices = STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date_created']
+        
+    def __str__(self):
+        return self.title
+    
