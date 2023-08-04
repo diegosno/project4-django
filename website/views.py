ef892113d7a36ff1d18f4bddfd64022d668af1ac
diegosno <dserranoleal@gmail.com>
2023-08-04 10:48:23 +0200

    Add contact view function

--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Home</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact</h1>')
