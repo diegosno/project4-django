95aa16b44b475761f73fac0dc53222c14ead0045
diegosno <dserranoleal@gmail.com>
2023-08-04 15:04:17 +0200

    Define a New model with fields for title, content, date_created, and author.

--- website/models.py
+++ website/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
 
-
-
-
 # Create your models here.
+
+class New(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    dated_created = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
