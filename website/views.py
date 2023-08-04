7fc38c071f17dd6716bfe03c45654715b34b49b1
diegosno <dserranoleal@gmail.com>
2023-08-04 13:58:38 +0200

    Create news dictionary in views.py

--- website/views.py
+++ website/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+news = [
+    {
+        'title': 'Summer',
+        'author': 'Diego',
+        'content': 'Summer content',
+        'date_created': 'July 12, 2022'
+        }
+    ,
+    {'title': 'Brunch',
+        'author': 'Dany',
+        'content': 'Brunch content',
+        'date_created': 'July 13, 2022'
+
+    }
+
+
+]
 def index(request):
-    return HttpResponse('<h1>Home</h1>')
+    context ={
+        'news': news
+    }
+    return render(request, 'website/index.html', context)
+
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, 'website/contact.html', {'title': 'Contact'})
