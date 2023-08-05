b25ccf5a18fb52116e729698e804cba322082631
diegosno <dserranoleal@gmail.com>
2023-08-05 12:01:39 +0200

    Specify the model and fields for the signUpForm in the Meta class.

--- website/forms.py
+++ website/forms.py
@@ -4,3 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class signUpForm(UserCreationForm):
     email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
