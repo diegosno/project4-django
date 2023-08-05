81d49687c4e71fc2935f898cc4632742f938aa36
diegosno <dserranoleal@gmail.com>
2023-08-05 13:28:00 +0200

    Add URL patterns for login and logout views using LoginView.as_view and LogoutView.as_view, specifying custom template files.

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='website-index'),
     path('contact/', views.contact, name='website-contact'),
     path('signup/', views.signUp, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='website/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='website/logout.html'), name='logout'),
 ]
