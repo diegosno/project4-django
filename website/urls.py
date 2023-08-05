31e3bdbb29fc0cb98c4ecf8f8281cd1c25ab999d
diegosno <dserranoleal@gmail.com>
2023-08-05 14:24:48 +0200

    Add a URL pattern for the 'profile' view from 'user_views'.

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views 
 from django.contrib.auth import views as auth_views
+from website import views as user_views
 
 
 urlpatterns = [
     path('', views.index, name='website-index'),
     path('contact/', views.contact, name='website-contact'),
     path('signup/', views.signUp, name='signup'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='website/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='website/logout.html'), name='logout'),
 ]
