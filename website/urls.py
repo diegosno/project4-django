06a366a846b3bdd20af97f17eb1c3733cf125c67
diegosno <dserranoleal@gmail.com>
2023-08-05 11:37:50 +0200

    Add signup URL route

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='website-index'),
     path('contact/', views.contact, name='website-contact'),
+    path('signup/', views.signUp, name='signup'),
 ]
