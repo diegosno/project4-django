bb7d5ae1a4fb496c9a41c0fb67e5442fea22b830
diegosno <dserranoleal@gmail.com>
2023-08-06 12:23:43 +0200

    Create Profile model with 'user' field as OneToOneField to User and 'picture' field as ImageField.

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
@@ -22,3 +23,6 @@ class New(models.Model):
     def __str__(self):
         return self.title
     
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(default='default.jpg', upload_to='user_pictures')
